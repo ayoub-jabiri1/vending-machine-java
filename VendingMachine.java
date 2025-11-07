567c710d20b75c64a4bae0570d2508679b4a2af7
Ayoub Jabiri <ayoubjabiri.coding@gmail.com>
2025-11-07 12:50:30 +0100

    Added the main arrays

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,8 @@
 public class VendingMachine {
     public static void main(String[] args) {
-        System.out.println("HEllo World");
+        String[] products = {"Eau", "Soda", "Chips", "Chocolat"};
+        double[] prix = {5.0, 8.0, 12.0, 15.0};
+        int[] stock = {10, 5, 7, 3};
+        
     }
 }
